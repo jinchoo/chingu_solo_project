12abaeba00a1c12eb434b4917d6d9cc4cdac7c34
Jin Choo <jinchoo315@gmail.com>
2022-09-25 23:05:34 -0500

    add question bank, add images for correct and incorrect answers check marks, add script file for UI implementation

---
+++ JS/question.js
@@ -0,0 +1,76 @@
+const quiz = [
+  {
+    question:
+      "Which HTML5 element should contain important links for navigating a website?",
+    options: ["li", "ul", "header", "nav"],
+    answer: 3,
+  },
+  {
+    question:
+      "Which HTML5 element should contain important information about what page you are on and the topic of the page?",
+    options: ["li", "ul", "header", "nav"],
+    answer: 2,
+  },
+  {
+    question:
+      "Which JavaScript variable declaration is used when the assigned value is likely to change?",
+    options: ["let", "var", "variable", "const"],
+    answer: 0,
+  },
+  {
+    question:
+      "Which term in JavaScript can be used to describe a declared variable that has not yet been given a value?",
+    options: ["null", "undefined", "typeError", "NaN"],
+    answer: 1,
+  },
+  {
+    question:
+      "Which selector could refer to an html element with the class of 'sm-col'?",
+    options: ["#sm-col", ".sm, .col", "[class~=col]", ".sm_col"],
+    answer: 2,
+  },
+  {
+    question:
+      "Setting an element height to '10vh' bases the height calculation on _________",
+    options: [
+      "viewfinder height",
+      "viewport width",
+      "visible height",
+      "viewport height",
+    ],
+    answer: 3,
+  },
+  {
+    question: "Which HTML5 property can be for custom data?",
+    options: ["data", "data-custom", "custom-data", "custom"],
+    answer: 0,
+  },
+  {
+    question:
+      "Which HTML5 tag is used to define 'independent, self-contained content' within a web page?",
+    options: ["section", "article", "aside", "div"],
+    answer: 1,
+  },
+  {
+    question:
+      "Which of the following snippets does not result in a return value of 8?",
+    options: ["8**1", "Math.pow(16, .75)", "2 * 2 * 'two'", "2**+'3'"],
+    answer: 2,
+  },
+  {
+    question:
+      "Which of these statements is NOT true about browser-side JavaScript?",
+    options: [
+      "JavaScript can be used for functional programming.",
+      "JavaScript can be used for file reading and writing on client machines.",
+      "JavaScript does not require frameworks or libraries to be used.",
+      "Though not class-based, JavaScript is an Object-Oriented Programming Language.",
+    ],
+    answer: 1,
+  },
+  {
+    question: "Who is the BEST??",
+    options: ["You", "Me", "Us", "Them"],
+    answer: 1,
+  },
+];
