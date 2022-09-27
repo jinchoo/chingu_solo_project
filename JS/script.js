91d48d9037dfacf09a4b2a3f4754d0e00dd58681
Jin Choo <jinchoo315@gmail.com>
2022-09-26 19:48:06 -0500

    implemented correct and wrong answers and the animation

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,7 @@ let questionCounter = 0;
 let currentQuestion;
 let availableQuestions = [];
 let availableOptions = [];
+
 //push the questions into availableQuestions Array
 function setAvailableQuestions() {
   const totalQuestion = quiz.length;
@@ -35,6 +36,7 @@ function getNewQuestion() {
   availableQuestions.splice(index1, 1);
   //console.log(questionIndex);
   //console.log(availableQuestions);
+
   // set options
   // get the length of options
   const optionLen = currentQuestion.options.length;
@@ -42,19 +44,65 @@ function getNewQuestion() {
   for (let i = 0; i < optionLen; i++) {
     availableOptions.push(i);
   }
+
+  optionContainer.innerHTML = "";
+  let animationDelay = 0.2;
   //create options in html
   for (let i = 0; i < optionLen; i++) {
+    // random option
+    const optionIndex =
+      availableOptions[Math.floor(Math.random() * availableOptions.length)];
+    // get the position of 'optionIndex' from the availableOptions
+    const index2 = availableOptions.indexOf(optionIndex);
+    // remove the 'optionIndex' from the availableOptions, so that the option does not repeat
+    availableOptions.splice(index2, 1);
+    //console.log(optionIndex);
+    //console.log(availableOptions);
     const option = document.createElement("div");
-    option.innerHTML = currentQuestion.options[i];
-    option.id = i;
+    option.innerHTML = currentQuestion.options[optionIndex];
+    option.id = optionIndex;
+    option.style.animationDelay = animationDelay + "s";
+    animationDelay = animationDelay + 0.2;
     option.className = "option";
     optionContainer.appendChild(option);
+    option.setAttribute("onclick", "getResult(this)");
   }
   //console.log(currentQuestion.options);
   //console.log(availableOptions);
   questionCounter++;
 }
 
+// get the result of current attempt question
+function getResult(element) {
+  const id = parseInt(element.id);
+  //console.log(typeof id);
+  // get the answer by comparing the id of clicked option
+  if (id === currentQuestion.answer) {
+    // set the green color to the correct option
+    element.classList.add("correct");
+    //console.log("answer is correct");
+  } else {
+    // set the red color to the incorrect option
+    element.classList.add("wrong");
+
+    // if the answer is incorrect the show the correct option by adding green color the correct option
+    const optionLen = optionContainer.children.length;
+    for (let i = 0; i < optionLen; i++) {
+      if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
+        optionContainer.children[i].classList.add("correct");
+      }
+    }
+    console.log("answer is wrong");
+  }
+  unclickableOptions();
+}
+// make all the options unclickable once the user select a option (RESTRICT THE USER TO CHANGE THE OPTION AGAIN)
+function unclickableOptions() {
+  const optionLen = optionContainer.children.length;
+  for (let i = 0; i < optionLen; i++) {
+    optionContainer.children[i].classList.add("already-answered");
+  }
+}
 function next() {
   if (questionCounter === quiz.length) {
     console.log("quiz over");
